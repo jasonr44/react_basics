044475b2eb13babecdd5cb6483ca5c7b05dc6f4e
jasonr44 <jasonr44@sbcglobal.net>
2018-05-16 23:40:15 -0400

    up to lecture 28, using switch

---
+++ routers/src/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, NavLink } from 'react-router-dom';
+
+// COMPONENTS
+import Home from './components/home';
+import Posts from './components/posts';
+import Profile from './components/profile';
+import PostItem from './components/post_item';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <header>
+            <NavLink to="/">Home</NavLink><br/>
+            <NavLink 
+            to="/posts"
+            activeStyle={{color:'red'}}
+            activeClassName="selected"
+            >Posts</NavLink><br/>
+            <NavLink to={{
+              pathname:'/profile'
+            }}>Profile</NavLink><br/>
+            <hr/>
+        </header>
+        <Route path="/" exact component={Home} />
+        <Route path="/posts" exact component={Posts} />
+        <Route path="/posts/:id/:username" component={PostItem} />
+        <Route path="/profile" component={Profile} />
+      </div>
+    </BrowserRouter>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+)
